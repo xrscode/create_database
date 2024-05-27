403d219f8f51ca50a3360acaec1b16c0b6be425a
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-27 21:51:09 +0100

    Updated Terraform Files

--- src/setup_schema.py
+++ src/setup_schema.py
@@ -1,1 +1,40 @@
 import pg8000
+import boto3
+import os
+from dotenv import load_dotenv
+
+# Get address of Database endpoint:
+
+
+def get_endpoint():
+    client = boto3.client('ssm')
+    response = client.get_parameter(
+        Name='db_endpoint'
+    )
+    return response['Parameter']['Value']
+
+
+# Assign Database Credentials:
+# db_host = get_endpoint()
+# db_user = os.getenv('DB_USER')
+# db_password = os.getenv('DB_PASSWORD')
+# db_name = os.getenv('DB_NAME')
+# db_port = os.getenv('DB_PORT')
+
+# print(db_host)
+# totesys.cfk2gikqsjhw.eu-west-2.rds.amazonaws.com
+
+
+credentials = {'host': 'totesys.cfk2gikqsjhw.eu-west-2.rds.amazonaws.com',
+               'database': 'totesys',
+               'password': 'password',
+               'user': 'postgres',
+               'port': 5432}
+
+try:
+    print('...attempting to connect')
+    con = pg8000.connect(**credentials)
+    print('Connected to the database!')
+    con.close()
+except Exception as e:
+    print(e)
--- terraform/database.tf
+++ terraform/database.tf
@@ -18,11 +18,21 @@ resource "aws_db_instance" "totesys" {
   username               = "postgres"
   password               = "password"
 #   db_subnet_group_name   = aws_db_subnet_group.education.name
-#   vpc_security_group_ids = [aws_security_group.rds.id]
+  vpc_security_group_ids = [aws_security_group.postgres_allow.id]
 #   parameter_group_name   = aws_db_parameter_group.education.name
-  publicly_accessible    = false
+  publicly_accessible    = true
+  
 # Allow Database to be deleted instantly:
   skip_final_snapshot    = true
+
+# Ensure Enhanced Monitoring is turned off
+  monitoring_interval    = 0
+
+# Assign Parameter Group
+  parameter_group_name = aws_db_parameter_group.totesys.name
+
+# Set Availability Zone:
+  availability_zone = "eu-west-2a" 
 }
 
 # Turns off SSL:
@@ -30,8 +40,41 @@ resource "aws_db_parameter_group" "totesys" {
     name = "nossl"
     family = "postgres16"
 
-    parameter{
+    parameter {
         name = "rds.force_ssl"
         value = 0
     }
 }
+
+# Store DB Endpoint in AWS Parameter Store
+resource "aws_ssm_parameter" "db_endpoint" {
+  name        = "db_endpoint"
+  description = "The endpoint of the RDS instance"
+  type        = "String"
+  value       = aws_db_instance.totesys.endpoint
+}
+
+# Create Security Group
+resource "aws_security_group" "postgres_allow" {
+  name = "postgres-rules"
+  description = "provides inbound/outbound rules"
+
+  # Set Inbound Rules:
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    # Allow connection from any ip:
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  # Set Outbound Rules:
+  egress {
+    from_port = 0
+    to_port = 0
+    # Allow all traffic:
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
